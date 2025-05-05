0e245f361e53db93581365c556761b943ed3881a
Pablo-rpg <81604980+Pablo-rpg@users.noreply.github.com>
2025-05-05 00:02:03 -0500

    Este es mi primer commit

---
+++ src/main/java/com/RestauranteWeb/restauranteweb/RestaurantewebApplication.java
@@ -0,0 +1,13 @@
+package com.RestauranteWeb.restauranteweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RestauranteWeb/restauranteweb/RestaurantewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestauranteWeb.restauranteweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
