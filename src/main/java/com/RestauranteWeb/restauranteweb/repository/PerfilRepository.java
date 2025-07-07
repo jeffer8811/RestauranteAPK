0f0c91fc208c322d66921f6bbc2dfeb7872ae470
Jeffer <73095499@certus.edu.pe>
2025-07-06 20:24:42 -0500

    Jeanpier promociones

--- src/main/java/com/RestauranteWeb/restauranteweb/repository/PerfilRepository.java
+++
@@ -1,7 +0,0 @@
-package com.RestauranteWeb.restauranteweb.repository;
-
-import com.RestauranteWeb.restauranteweb.model.Perfil;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerfilRepository extends JpaRepository<Perfil, Long> {
-}
